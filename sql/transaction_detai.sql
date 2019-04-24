9e3d1f73004b0e6fb8292d6fd0fac5723c96ff21
ph680580 <pangheng000>
2019-04-24 20:07:10 +0800

    add sql and xml

--- sql/transaction_detai.sql
+++ sql/transaction_detai.sql
@@ -6,8 +6,8 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `transaction_detail`;
 CREATE TABLE `transaction_detail` (
   `txid` char(64) NOT NULL,
-  `address` varchar(70) NOT NULL ,
-  `type` tinyint NOT NULL,
+  `address` varchar(70)  ,
+  `type` tinyint ,
   `amount` double,
   PRIMARY KEY (`txid`,`address`),
   index `idx_address`(`address`)
