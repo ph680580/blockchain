3c898c194f8e513ba97a8e5e70790d5aa3261ee1
ph680580 <pangheng000>
2019-04-25 16:12:01 +0800

    add addblockController

--- src/main/java/com/ph/blockchainexplorer/controller/BlockController.java
+++ src/main/java/com/ph/blockchainexplorer/controller/BlockController.java
@@ -1,5 +1,6 @@
 package com.ph.blockchainexplorer.controller;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.ph.blockchainexplorer.blockapi.BitcoinApi;
 import com.ph.blockchainexplorer.blockapi.BitcoinJsonRpcClient;
@@ -11,8 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.sql.Date;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -37,20 +37,39 @@ public class BlockController {
     @GetMapping("/getRecentBlocks")
     public List<BlockListDTO> getRecentBlocks() throws Throwable {
 
-        String bestBlockhash = bitcoinJsonRpcClient.getBestBlockhash();
-        String bestBlockhash1 = bestBlockhash;
-        List<BlockListDTO> blockListDTOS =new LinkedList<>();
-        for(int i =0;i<10;i++){
-            JSONObject block =bitcoinApi.getNoTxBlock(bestBlockhash1);
+//        String bestBlockhash = bitcoinJsonRpcClient.getBestBlockhash();
+//        String bestBlockhash1 = bestBlockhash;
+//        List<BlockListDTO> blockListDTOS =new LinkedList<>();
+//        for(int i =0;i<10;i++){
+//            JSONObject block =bitcoinApi.getNoTxBlock(bestBlockhash1);
+//            BlockListDTO blockListDTO = new BlockListDTO();
+//            blockListDTO.setHeight(block.getInteger("height"));
+//            Long time = block.getLong("time");
+//            Date date = new Date(time * 1000);
+//            blockListDTO.setTime(date);
+//            blockListDTO.setTxSize(block.getJSONArray("tx").size());
+//            blockListDTO.setSizeOnDisk(block.getLong("size"));
+//            blockListDTOS.add(blockListDTO);
+//            bestBlockhash1 = block.getString("previousblockhash");
+//        }
+
+        JSONObject chainInfo = bitcoinApi.getChainInfo();
+        Integer height = chainInfo.getInteger("blocks");
+        height -= 6;
+        String blockHashByHeight = bitcoinJsonRpcClient.getBlockHashByHeight(height);
+        JSONArray blockHeaders = bitcoinApi.getBlockHeaders(5, blockHashByHeight);
+
+        LinkedList<BlockListDTO> blockListDTOS = new LinkedList<>();
+        for (int i = 5; i > -1; i--) {
+            JSONObject block = blockHeaders.getJSONObject(i);
             BlockListDTO blockListDTO = new BlockListDTO();
             blockListDTO.setHeight(block.getInteger("height"));
             Long time = block.getLong("time");
             Date date = new Date(time * 1000);
             blockListDTO.setTime(date);
-            blockListDTO.setTxSize(block.getJSONArray("tx").size());
-            blockListDTO.setSizeOnDisk(block.getLong("size"));
+            //todo add size on disk
+            blockListDTO.setTxSize(block.getInteger("nTx"));
             blockListDTOS.add(blockListDTO);
-            bestBlockhash1 = block.getString("previousblockhash");
         }
 
         return blockListDTOS;
